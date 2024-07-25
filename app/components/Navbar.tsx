56659483a015c9f7f30e09f5b775fe71880fdbe2
Sahil Mukesh Ambre <59484827+sahilambre@users.noreply.github.com>
2024-07-25 01:53:24 -0400

    :building_construction: initial commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto text-center">
+        <span className="text-white text-2xl">AI Travel Assitant</span>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
